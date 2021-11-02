423063699fea2bbb4e422ccecaff139985156a0c
Dan Shiebler <danshiebler@Dans-MacBook.fios-router.home>
2021-11-02 11:20:16 -0400

    fix

--- _site/test.sh
+++ _site/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # To push to git, run `npm run deploy` before git push
-bundle exec jekyll serve
+cd dev-awesome-tool; npm run deploy; cd ..; bundle exec jekyll serve
--- dev-awesome-tool/src/index.js
+++ dev-awesome-tool/src/index.js
@@ -2,22 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// class Square extends React.Component {
+import $ from 'jquery';
 
-//   render() {
-//     return (
-//       // DOM button element, onClick is a reserved method
-//       <button className="square" 
-//         // lambda syntax
-//         onClick={() => this.props.onClick()}>
-//         {this.props.value}
-//       </button>
-//     );
-//   }
-// }
+
 
 // We can switch the Square component to a function that just renders directly. This still creates a component I believe, just under the hood
-function Square(props) {
+function Button(props) {
   // the function component just returns the DOM element to render
   return (
     // 
@@ -27,122 +17,105 @@ function Square(props) {
     // (2)
     //  - For class component we write onClick={() => this.props.onClick}
     //  - For function component we write onClick={props.onClick}
-    <button className="square" onClick={props.onClick}>
-      {props.value}
+    <button className="button" onClick={props.onClick}>
+       {"Click Here"}
     </button>
   );
 }
 
-class Board extends React.Component {
+function Response(props) {
+  // the function component just returns the DOM element to render
+
+  return (
+    <div className="response">{props.response}</div>
+  );
+}
+
+
+class Screen extends React.Component {
+  "use strict"
 
   constructor(props) {
     super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true
-    };
+    this.state = {response: null};
   }
 
 
-  handleClick(i) {
-    // copy the squares attribute
-    const squares = this.state.squares.slice();
+  handleResponse(screen, response) {
+    console.log(screen);
+    console.log(this);
+    console.log("setting state");
 
-    // Don't do anything if there is already a winner or this square is already taken
-    if (!calculateWinner(squares) && !squares[i]) {
-      // edit the cop/y
-      squares[i] = this.state.xIsNext ? 'X' : 'O';
-
-      // Set the this.state attribute manually with a setter. This is based on good React practice (don't mutate properties directly)
-      this.setState({
-        squares: squares,
-        xIsNext: !this.state.xIsNext,
-      })
-    }
+    // NOTE: If we write this method to only accept the response argument, then `this` will point at the response object. We need to pass the screen in manually. 
+    screen.setState({ response: response });
+    return response;
   }
 
-  renderSquare(i) {
-    return <Square
-      value={this.state.squares[i]}
-      // this is just the name of the method that we are creating Square with, this does not need to be called onClick
-      onClick={() => this.handleClick(i)}
+  handleClick() {
+    $.getJSON('https://randomuser.me/api/')
+      .done((response) => this.handleResponse(this, response));
+  }
+
+  renderButton() {
+    return <Button
+      onClick={() => this.handleClick()}
       />;
   }
 
-  render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
+  renderResponse() {
+    // console.log(this.state.response)
+
+    var response;
+    if (this.state.response) {
+      console.log("defined")
+      const persons = this.state.response.results.map((item, i) => (
+        // NOTE: We need to wrap these within <li><\li> and add a key because of https://sentry.io/answers/unique-key-prop/
+        <li key={"person_" + i}> 
+        <div>
+          <h1>{ item.name.first }</h1>
+          <span>{ item.cell }, { item.email }</span>
+        </div>
+        </li>
+      ));
+
+      console.log(persons);
+
+      response = (
+        <div id="layout-content" className="layout-content-wrapper">
+          <div className="panel-list">{ persons }</div>
+        </div>
+      );
+
+      // response = JSON.stringify(this.state.response);
     } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      console.log("not defined")
+      response = null;
     }
 
+    return <Response
+        response={response}
+        />;
+  }
+
+  render() {
+    console.log("rendering");
     return (
       <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+        <div className="box">
+          {this.renderButton()}
         </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+        <div className="response-box">
+          {this.renderResponse()}
         </div>
       </div>
     );
   }
 }
 
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
 
 // ========================================
 
 ReactDOM.render(
-  <Game />,
+  <Screen />,
   document.getElementById('root')
 );
